37950c374435ce68c5a9f32c47adcdfe45ce027f
Krzysztof Gabis <kgabis@gmail.com>
2013-04-28 19:33:35 +0200

    Initial Commit

---
+++ raytracer_cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  raytracer_cpp
+//
+//  Created by Krzysztof Gabis on 28.04.2013.
+//  Copyright (c) 2013 Krzysztof Gabis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
